2d81730936ca64cacc41e0932246c7bbd2f3dc68
Jochen Klar <mail@jochenklar.de>
2019-06-12 15:44:04 +0200

    Add header and assets

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -17,7 +18,7 @@ module.exports = {
     libraryTarget: 'this',
     library: '[name]',
     path: path.resolve('./assets/'),
-    publicPath: '/assets/',
+    publicPath: '/static/',
     filename: '[name].js'
   },
   module: {
@@ -30,12 +31,33 @@ module.exports = {
           'sass-loader'
         ]
       },
+      {
+        test: /(fonts|files)\/.*\.(svg|woff2?|ttf|eot|otf)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.svg$|\.png$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        }
+      }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: '**/*',
+        to: './/images/',
+        context: './isimip_data/core/assets/core/images/',
+      }
+    ])
   ]
 }
