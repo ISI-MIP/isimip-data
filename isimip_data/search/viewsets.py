6137d56dc894f1a91bd6ace5a25092198d4b1e9b
Jochen Klar <mail@jochenklar.de>
2019-08-15 15:58:33 +0200

    Add facets action to DatasetViewSet

--- isimip_data/search/viewsets.py
+++ isimip_data/search/viewsets.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.db.models import Count
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 from isimip_data.metadata.models import Dataset, File
 
@@ -26,10 +32,18 @@ class DatasetViewSet(ReadOnlyModelViewSet):
     )
     filterset_fields = (
         'name',
-        'version',
-        'checksum'
+        'version'
     )
 
+    @action(detail=False, url_path='facets/(?P<attribute>[A-Za-z0-9_]+)')
+    def facets(self, request, attribute):
+        if attribute not in settings.ATTRIBUTES_FILTER:
+            raise NotFound
+        else:
+            field = 'attributes__%s' % attribute
+            queryset = self.filter_queryset(self.get_queryset())
+            values = queryset.values_list(field).annotate(count=Count(field)).order_by(field)
+            return Response(values)
 
 class FileViewSet(ReadOnlyModelViewSet):
 
