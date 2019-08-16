f2f606f5d8f61f86ef790f4748ae9ada961fd441
Jochen Klar <mail@jochenklar.de>
2019-08-16 11:28:58 +0200

    Add Pagination component

---
+++ isimip_data/search/assets/js/components/Pagination.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const range = (start, end) => {
+  return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
+class Pagination extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e, page) {
+    e.preventDefault()
+    const { onSubmit } = this.props
+    onSubmit(page)
+  }
+
+  render() {
+    const { count, page, pageSize, onSubmit } = this.props
+    const pageCount = Math.ceil(count / pageSize)
+
+    const startPage = Math.max(1, Math.min(page - 5, pageCount - 10))
+    const endPage = Math.min(Math.max(page + 5, 10), pageCount)
+
+    const pages = range(startPage, endPage)
+    const items = pages.map(currentPage => {
+      return (
+        <li className={'page-item' + ((currentPage == page) ? ' active' : '')} key={currentPage}>
+          <a className="page-link" href=""
+              onClick={e => this.handleClick(e, currentPage)}>
+            {currentPage}
+          </a>
+        </li>
+      )
+    })
+
+    return (
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className={'page-item' + ((page == startPage) ? ' disabled' : '')}>
+            <a className="page-link" href="" onClick={e => this.handleClick(e, page - 1)}>
+              Previous
+            </a>
+          </li>
+          {items}
+          <li className={'page-item' + ((page == endPage) ? ' disabled' : '')}>
+            <a className="page-link" href="" onClick={e => this.handleClick(e, page + 1)}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+Pagination.propTypes = {
+  count: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default Pagination
--- isimip_data/search/assets/js/containers/App.js
+++ isimip_data/search/assets/js/containers/App.js
@@ -4,44 +4,68 @@ import PropTypes from 'prop-types'
 import DatasetApi from '../api/DatasetApi'
 import Search from '../components/Search'
 import Results from '../components/Results'
+import Pagination from '../components/Pagination'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      query: null,
+      params: {
+        search: '',
+        page: 1
+      },
       datasets: {
+        count: 0,
         results: []
       }
     };
     this.handleSearch = this.handleSearch.bind(this);
+    this.handlePagination = this.handlePagination.bind(this)
   }
 
   componentDidMount() {
     this.fetch()
   }
 
-  fetch() {
-    const { query } = this.state
+  reset() {
+    this.setState({
+      params: {
+        search: '',
+        page: 1
+      }
+    })
+  }
 
-    let params = {}
-    if (query) {
-      params.search = query
-    }
+  fetch() {
+    const { params } = this.state
 
     DatasetApi.fetchDatasets(params).then(datasets => {
       this.setState({ datasets })
     })
   }
 
-  handleSearch(query) {
+  handleSearch(search) {
+    const { params } = this.state
+
+    params.search = search
+    params.page = 1
+
     // update state and call fetch as callback
-    this.setState({ query }, this.fetch)
+    this.setState({ params }, this.fetch)
+  }
+
+  handlePagination(page) {
+    const { params } = this.state
+
+    params.page = page
+
+    // update state and call fetch as callback
+    this.setState({ params }, this.fetch)
   }
 
   render() {
-    const { datasets } = this.state
+    const { params, datasets } = this.state
 
     return (
       <div className="row">
@@ -52,6 +76,7 @@ class App extends Component {
 
         </div>
         <div className="col-lg-9">
+          <Pagination count={datasets.count} page={params.page} pageSize={10} onSubmit={this.handlePagination} />
           <Results results={datasets.results} />
         </div>
       </div>
