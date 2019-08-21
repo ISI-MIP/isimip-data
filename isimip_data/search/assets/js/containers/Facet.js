14496382de5ed40a16992cc4f12e4706deabfa48
Jochen Klar <mail@jochenklar.de>
2019-08-21 18:18:42 +0200

    Fix facet lists for firefox

--- isimip_data/search/assets/js/containers/Facet.js
+++ isimip_data/search/assets/js/containers/Facet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp, faChevronDown, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 import DatasetApi from '../api/DatasetApi'
 
@@ -19,7 +19,7 @@ class Facet extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.params !== prevProps.params) {
+    if (open && this.props.params !== prevProps.params) {
       this.fetch()
     }
   }
@@ -47,45 +47,54 @@ class Facet extends Component {
     this.setState({ open: !open }, this.fetch)
   }
 
+  renderListGroup(items, checked) {
+    if (items.length > 0) {
+      return (
+        <ul className="list-group list-group-flush">
+          {
+            items.map((item, index) => {
+              const [key, count] = item
+              const id = item + '-facet-' + index
+              const isChecked = (checked.indexOf(key) > -1)
+
+              if (key !== null) {
+                return (
+                  <li key={index} className="list-group-item facet-item d-flex justify-content-between align-items-center">
+                    <label className="form-check-label" htmlFor={id}>
+                      <input type="checkbox" className="form-check-input" id={id}
+                             checked={isChecked} onChange={e => this.handleChange(key, e)} /> {key}
+                    </label>
+                    <span className="badge badge-secondary badge-pill pull-right">
+                      {count}
+                    </span>
+                  </li>
+                )
+              }
+            })
+          }
+        </ul>
+      )
+    } else {
+      return (
+        <div class="card-body text-center">
+          <FontAwesomeIcon icon={faSpinner} spin />
+        </div>
+      )
+    }
+  }
+
   render() {
     const { params, facet } = this.props
     const { open, items } = this.state
     const checked = params[facet.attribute] || []
 
-    let list_group
-    if (open) {
-      list_group = items.map((item, index) => {
-        const [key, count] = item
-        const id = item + '-facet-' + index
-        const isChecked = (checked.indexOf(key) > -1)
-
-        if (key !== null) {
-          return (
-            <li key={index} className="list-group-item facet-item d-flex justify-content-between align-items-center">
-              <label className="form-check-label" htmlFor={id}>
-                <input type="checkbox" className="form-check-input" id={id} checked={isChecked}
-                    onChange={e => this.handleChange(key, e)} /> {key}
-              </label>
-              <span className="badge badge-secondary badge-pill pull-right">
-                {count}
-              </span>
-            </li>
-          )
-        }
-      })
-    }
-
     return (
       <div className="card facet small">
-        <div className="card-header d-flex justify-content-between align-items-center">
+        <div className="card-header d-flex justify-content-between align-items-center"  onClick={this.toggleFacet}>
           {facet.title}
-          <button type="button" className="btn btn-sm" onClick={this.toggleFacet}>
-            {open ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
-          </button>
+          {open ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
         </div>
-        <ul className="list-group list-group-flush">
-          {list_group}
-        </ul>
+        {open ? this.renderListGroup(items, checked) : ''}
       </div>
     )
   }
