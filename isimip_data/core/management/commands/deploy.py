35195e2e582759df0d7187e069c20acb3edb1fb3
Jochen Klar <mail@jochenklar.de>
2019-08-28 16:08:38 +0200

    Update deploy script

--- isimip_data/core/management/commands/deploy.py
+++ isimip_data/core/management/commands/deploy.py
@@ -7,5 +7,5 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        subprocess.call(['/bin/bash', '-i', '-c', 'nvm install; npm install; npm run build:prod'])
         call_command('collectstatic', '--noinput')
-        subprocess.call(['touch', 'config/wsgi.py'])
