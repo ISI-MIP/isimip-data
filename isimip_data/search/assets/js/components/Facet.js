3c99fdead01a6876ead5a1d4df46632015164024
Jochen Klar <mail@jochenklar.de>
2019-08-28 16:40:18 +0200

    Refactor facets

--- isimip_data/search/assets/js/components/Facet.js
+++ isimip_data/search/assets/js/components/Facet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronUp, faChevronDown, faSpinner, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp, faChevronDown, faSpinner, faCheckSquare, faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 
 import DatasetApi from '../api/DatasetApi'
 
@@ -11,7 +11,8 @@ class Facet extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      open: false,
+      isOpen: false,
+      isLoading: false,
       items: []
     }
     this.handleChange = this.handleChange.bind(this)
@@ -19,19 +20,21 @@ class Facet extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (open && this.props.params !== prevProps.params) {
+    if (this.state.isOpen && this.props.params !== prevProps.params) {
       this.fetch()
     }
   }
 
   fetch() {
     const { params, facet } = this.props
-    const { open } = this.state
+    const { isOpen } = this.state
 
-    if (open) {
+    if (isOpen) {
+      this.setState({ isLoading: true })
       DatasetApi.fetchDatasetsFacets(facet.attribute, params).then(items => {
         this.setState({
-          items
+          isLoading: false,
+          items: items
         })
       })
     }
@@ -43,55 +46,62 @@ class Facet extends Component {
   }
 
   toggleFacet() {
-    const { open } = this.state
-    this.setState({ open: !open }, this.fetch)
+    const { isOpen } = this.state
+    this.setState({ isOpen: !isOpen }, this.fetch)
   }
 
   renderListGroup(items, checked) {
-    if (items.length > 0) {
-      return (
-        <ul className="list-group list-group-flush">
-          {
-            items.map((item, index) => {
-              const [key, count] = item
-              const id = item + '-facet-' + index
-              const isChecked = (checked.indexOf(key) > -1)
+    return (
+      <ul className="list-group list-group-flush">
+        {
+          items.map((item, index) => {
+            const [key, count] = item
+            const id = item + '-facet-' + index
+            const isChecked = (checked.indexOf(key) > -1)
 
-              if (key !== null) {
-                return (
-                  <li key={index} className="list-group-item facet-item d-flex justify-content-between align-items-center">
-                    <label className="form-check-label" htmlFor={id}>
-                      <input type="checkbox" className="form-check-input" id={id}
-                             checked={isChecked} onChange={e => this.handleChange(key, e)} /> {key}
-                    </label>
-                    <span className="badge badge-secondary badge-pill pull-right">
-                      {count}
-                    </span>
-                  </li>
-                )
-              }
-            })
-          }
-        </ul>
-      )
-    } else {
-      return (
-        <div className="card-body text-center">
-          <FontAwesomeIcon icon={faSpinner} spin />
-        </div>
-      )
-    }
+            if (key !== null) {
+              return (
+                <li key={index} className="list-group-item facet-item d-flex justify-content-between align-items-center">
+                  <label className="form-check-label" htmlFor={id}>
+                    <input type="checkbox" className="form-check-input" id={id}
+                           checked={isChecked} onChange={e => this.handleChange(key, e)} /> {key}
+                  </label>
+                  <span className="badge badge-secondary badge-pill pull-right">
+                    {count}
+                  </span>
+                </li>
+              )
+            }
+          })
+        }
+      </ul>
+    )
+  }
+
+  renderEmpty() {
+    return (
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          <FontAwesomeIcon icon={faEllipsisH} />
+        </li>
+      </ul>
+    )
+  }
+
+  renderSpinner() {
+    return (
+      <div className="card-body text-center">
+        <FontAwesomeIcon icon={faSpinner} spin />
+      </div>
+    )
   }
 
   render() {
     const { params, facet } = this.props
-    const { open, items } = this.state
+    const { isOpen, isLoading, items } = this.state
     const checked = params[facet.attribute] || []
-
-    let check
-    if (checked.length) {
-      check = <FontAwesomeIcon className="facet-check text-secondary" icon={faCheckSquare} />
-    }
+    const isChecked = checked.length > 0
+    const isEmpty = items.length == 0
 
     return (
       <div className="card facet">
@@ -99,11 +109,13 @@ class Facet extends Component {
           {facet.title}
 
           <div>
-            {check}
-            {open ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
+            {isChecked && <FontAwesomeIcon className="facet-check text-secondary" icon={faCheckSquare} />}
+            {isOpen ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
           </div>
         </div>
-        {open ? this.renderListGroup(items, checked) : ''}
+        {isOpen && !isEmpty && this.renderListGroup(items, checked)}
+        {isOpen && isEmpty && !isLoading && this.renderEmpty()}
+        {isOpen && isEmpty && isLoading && this.renderSpinner()}
       </div>
     )
   }
