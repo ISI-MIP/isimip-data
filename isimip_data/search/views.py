0cd23326f589bbc5f7c4c1daf6046a38a908a9d3
Jochen Klar <mail@jochenklar.de>
2020-07-14 16:44:32 +0200

    Refactor home page

--- isimip_data/search/views.py
+++ isimip_data/search/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def search(request, path=None):
+    # redirect GET params to match react location
+    if request.GET:
+        url = request.path.rstrip('/')
+        for key in request.GET:
+            for value in request.GET.getlist(key):
+                url += '/{}/{}'.format(key, value)
+
+        return redirect(url)
+
     return render(request, 'search/search.html')
