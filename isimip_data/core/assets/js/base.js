e58143ad0fc0589fb6ade535223aaab7710e8c13
Jochen Klar <mail@jochenklar.de>
2021-05-05 14:46:48 +0200

    Refactor resource page

--- isimip_data/core/assets/js/base.js
+++ isimip_data/core/assets/js/base.js
@@ -1,5 +1,11 @@
-import 'bootstrap';
+import 'bootstrap'
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+
+  $('.btn-copy-to-clipboard').click(function () {
+    var code = this.getElementsByTagName('code')[0];
+    var text = code.textContent;
+    navigator.clipboard.writeText(text);
+  })
 })
