c2b19e9911454cb247bed53c958d0ec5d3668850
Jochen Klar <mail@jochenklar.de>
2021-07-27 17:37:36 +0200

    Fix tests for Django 3.2

--- conftest.py
+++ conftest.py
@@ -8,6 +8,10 @@ from django.core.management import call_command
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
+    from django.test import TestCase
+    TestCase.multi_db = True
+    TestCase.databases = ('default', 'metadata')
+
     with django_db_blocker.unblock():
         fixtures = flatten([os.listdir(fixture_dir) for fixture_dir in settings.FIXTURE_DIRS])
         call_command('loaddata', *fixtures)
