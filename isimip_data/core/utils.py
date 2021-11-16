f57490e9aa9e88c44bfe1fada5d8f9ce8ad3f71f
Jochen Klar <mail@jochenklar.de>
2021-11-16 17:22:26 +0100

    Fix PROXY setting again

--- isimip_data/core/utils.py
+++ isimip_data/core/utils.py
@@ -1,20 +1,14 @@
 from django.conf import settings
 
 
-def is_using_proxy(request):
+def via_proxy(request):
     remote_addr = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
     return settings.PROXY and (remote_addr in settings.PROXY)
 
 
 def get_file_base_url(request):
-    if is_using_proxy(request):
-        return settings.PROXY_FILES_BASE_URL.rstrip('/') + '/'
-    else:
-        return settings.FILES_BASE_URL.rstrip('/') + '/'
+    return settings.PROXY_FILES_BASE_URL if via_proxy(request) else settings.FILES_BASE_URL
 
 
 def get_file_api_url(request):
-    if is_using_proxy(request):
-        return settings.PROXY_FILES_API_URL.rstrip('/') + '/'
-    else:
-        return settings.FILES_API_URL.rstrip('/') + '/'
+    return settings.PROXY_FILES_API_URL if via_proxy(request) else settings.FILES_API_URL
