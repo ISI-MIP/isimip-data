bba8760b6aaae60aef96de65020d451fe3b9e4e2
Jochen Klar <mail@jochenklar.de>
2021-11-16 14:36:50 +0100

    Fix ensure CSRF token

--- isimip_data/download/views.py
+++ isimip_data/download/views.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-@ensure_csrf_cookie
 def download(request, job_id=None):
     return render(request, 'download/download.html', {
         'job_url': settings.FILES_API_URL + job_id if job_id else None,
