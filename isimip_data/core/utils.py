e0648ecb6eb1a3ca148bd7938b41df663debf77d
Jochen Klar <mail@jochenklar.de>
2021-11-17 10:16:02 +0100

    Add trailing_slash function

--- isimip_data/core/utils.py
+++ isimip_data/core/utils.py
@@ -1,14 +1,20 @@
 from django.conf import settings
 
 
+def trailing_slash(string):
+    return string.rstrip('/') + '/'
+
+
 def via_proxy(request):
     remote_addr = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
     return settings.PROXY and (remote_addr in settings.PROXY)
 
 
 def get_file_base_url(request):
-    return settings.PROXY_FILES_BASE_URL if via_proxy(request) else settings.FILES_BASE_URL
+    url = settings.PROXY_FILES_BASE_URL if via_proxy(request) else settings.FILES_BASE_URL
+    return trailing_slash(url)
 
 
 def get_file_api_url(request):
-    return settings.PROXY_FILES_API_URL if via_proxy(request) else settings.FILES_API_URL
+    url = settings.PROXY_FILES_API_URL if via_proxy(request) else settings.FILES_API_URL
+    return trailing_slash(url)
