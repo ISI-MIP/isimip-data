be5ed02a3f84f93fd381c36642f8af9e5a5db464
Jochen Klar <mail@jochenklar.de>
2022-02-03 12:24:16 +0100

    Add watch management script

---
+++ isimip_data/core/management/commands/watch.py
@@ -0,0 +1,10 @@
+import subprocess
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        subprocess.call(['/bin/bash', '-i', '-c', 'npm run watch'])
