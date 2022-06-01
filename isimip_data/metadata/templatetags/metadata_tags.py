240ae167da2811ff11228d7e3eaa3896ee7fb183
Jochen Klar <mail@jochenklar.de>
2022-06-01 15:38:42 +0200

    Refactor related identifiers

--- isimip_data/metadata/templatetags/metadata_tags.py
+++ isimip_data/metadata/templatetags/metadata_tags.py
@@ -9,11 +9,8 @@ def citation(citation, identifier, identifier_type):
     string = citation
 
     if identifier_type == 'DOI':
-        string = string.replace(identifier, '')
-        string += ' <a class="doi-link" target="_blank" href="{}">{}</a>'.format(identifier_type, identifier)
+        string = string.replace(identifier, '<a class="doi-link" target="_blank" href="{}">{}</a>'.format(identifier_type, identifier))
     elif identifier_type == 'URL':
-        string = string.replace(identifier, '')
-        string += ' &nbsp;&rarr;'
-        string += ' <a target="_blank" href="{}">{}</a>'.format(identifier_type, identifier)
+        string = string.replace(identifier, '&nbsp;&rarr; <a target="_blank" href="{}">{}</a>'.format(identifier_type, identifier))
 
     return mark_safe(string)
