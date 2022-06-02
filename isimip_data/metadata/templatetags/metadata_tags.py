5a18ac61fecd6c09dbc2b9243776fe913815f8ea
Jochen Klar <mail@jochenklar.de>
2022-06-02 12:00:03 +0200

    Fix resource template

--- isimip_data/metadata/templatetags/metadata_tags.py
+++ isimip_data/metadata/templatetags/metadata_tags.py
@@ -9,8 +9,8 @@ def citation(citation, identifier, identifier_type):
     string = citation
 
     if identifier_type == 'DOI':
-        string = string.replace(identifier, '<a class="doi-link" target="_blank" href="{}">{}</a>'.format(identifier_type, identifier))
+        string = string.replace(identifier, '<a class="doi-link" target="_blank" href="{}">{}</a>'.format(identifier, identifier))
     elif identifier_type == 'URL':
-        string = string.replace(identifier, '&nbsp;&rarr; <a target="_blank" href="{}">{}</a>'.format(identifier_type, identifier))
+        string = string.replace(identifier, '&nbsp;&rarr; <a target="_blank" href="{}">{}</a>'.format(identifier, identifier))
 
     return mark_safe(string)
