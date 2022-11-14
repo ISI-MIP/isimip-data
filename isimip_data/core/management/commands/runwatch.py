65aff83fbeeb1ed89b0b552a1bade153911a7d96
Jochen Klar <mail@jochenklar.de>
2022-11-14 14:04:03 +0100

    Rename management script watch -> runwatch

--- isimip_data/core/management/commands/runwatch.py
+++ isimip_data/core/management/commands/runwatch.py
@@ -1,10 +1,9 @@
 import subprocess
 
-from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        subprocess.call(['/bin/bash', '-i', '-c', 'npm run watch'])
+        subprocess.call(['/bin/bash', '-i', '-c', 'nvm use; npm run watch'])
