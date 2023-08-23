31fcfaacaf34276b7e32acdf3869e1f8c334ac9f
Jochen Klar <mail@jochenklar.de>
2023-08-23 17:51:12 +0200

    Update deploy management script

--- isimip_data/core/management/commands/deploy.py
+++ isimip_data/core/management/commands/deploy.py
@@ -7,6 +7,6 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        subprocess.call(['/bin/bash', '-i', '-c', 'npm install; npm run build:prod'])
+        subprocess.call(['/bin/bash', '-i', '-c', 'npm ci; npm run build:prod'])
         call_command('collectstatic', '--noinput', '--clear')
         call_command('clear_cache')
