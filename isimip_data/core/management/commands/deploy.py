b8c73746ce94d9578624abf43b7dff10cb953212
Jochen Klar <mail@jochenklar.de>
2023-08-23 17:22:17 +0200

    Add clear_cache to deploy management script

--- isimip_data/core/management/commands/deploy.py
+++ isimip_data/core/management/commands/deploy.py
@@ -9,3 +9,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         subprocess.call(['/bin/bash', '-i', '-c', 'npm install; npm run build:prod'])
         call_command('collectstatic', '--noinput', '--clear')
+        call_command('clear_cache')
