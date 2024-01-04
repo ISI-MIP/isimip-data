4218eefaf85da9d8a8713c20c2ca032a527a7a7f
Jochen Klar <mail@jochenklar.de>
2024-01-04 18:44:25 +0100

    Fix suggestion list when suggestions are empty

--- isimip_data/search/assets/js/components/Suggestions.js
+++ isimip_data/search/assets/js/components/Suggestions.js
@@ -19,16 +19,20 @@ class Suggestions extends Component {
 
   render() {
     const { count, suggestions, onClick } = this.props
+    const suggestionsList = suggestions === null ? [] :suggestions.reduce((cur, agg) => {
+      if (cur.length > 0) agg.push(cur)
+      return agg
+    }, [])
 
     return (count == 0) && (
       <div className="card suggestions">
         <div className="card-body">
           <strong>No results found</strong>
           {
-            suggestions && <>
+            suggestionsList.length > 0 && <>
               <span className="ml-2">Maybe you misspelled your query. Did you mean:</span>
               {
-                suggestions.map((suggestion, index) => (
+                suggestionsList.map((suggestion, index) => (
                   <button key={index} className="btn btn-link ml-2" onClick={() => onClick(suggestion)}>
                     {suggestion}
                   </button>
