23dd248fc2026fa8282d677fec64a2183d14e051
Jochen Klar <mail@jochenklar.de>
2024-03-27 10:23:06 +0100

    Remove runwatch script

--- isimip_data/core/management/commands/runwatch.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        subprocess.call(['/bin/bash', '-i', '-c', 'nvm use; npm run watch'])
