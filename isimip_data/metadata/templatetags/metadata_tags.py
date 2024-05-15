febec5dbfa69ed0b979893ab40b10442edc67320
Jochen Klar <mail@jochenklar.de>
2024-05-15 16:53:46 +0200

    Add replace_caveats template filter

--- isimip_data/metadata/templatetags/metadata_tags.py
+++ isimip_data/metadata/templatetags/metadata_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
@@ -24,3 +26,10 @@ def restricted_message(obj):
             return mark_safe(message)
 
     return mark_safe(settings.RESTRICTED_DEFAULT_MESSAGE)
+
+
+@register.filter
+def replace_caveats(value):
+    # replaces, e.g. <a ...>https://data.isimip.org/caveats/8/</a>
+    # with <a ...>#8</a>
+    return mark_safe(re.sub(r'(<a.*?>)(.*?)(\d+)([/]*)(</a>)', r'\1#\3\5', value))
