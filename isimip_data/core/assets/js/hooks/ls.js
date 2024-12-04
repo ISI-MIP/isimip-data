ebc3fba2b4c95cb6b9683f14d32542da0149ed0f
Jochen Klar <mail@jochenklar.de>
2024-12-04 13:46:45 +0100

    Fix local storage in download

--- isimip_data/core/assets/js/hooks/ls.js
+++ isimip_data/core/assets/js/hooks/ls.js
@@ -1,37 +1,30 @@
 import { useState } from 'react'
 import { difference, isArray, isEmpty, isNaN, isNil, isPlainObject, toNumber } from 'lodash'
 
-const deserialize = (values) => {
-  if (isArray(values)) {
-    return values.map(value => deserialize(value))
-  } else if (isPlainObject(values)) {
-    if (isEmpty(difference(Object.keys(values), ['path']))) {
-      return loadFile(values.path) // this is a file
+const deserialize = (value) => {
+  if (isArray(value)) {
+    return value.map(v => deserialize(v))
+  } else if (isPlainObject(value)) {
+    if (!isEmpty(value.fileName)) {
+      return loadFile(value.fileName) // this is a file
     } else {
-      return Object.fromEntries(Object.entries(values).map(([key, value]) => [key, deserialize(value)]))
+      return Object.fromEntries(Object.entries(value).map(([k, v]) => [k, deserialize(v)]))
     }
-  } else if (['', true, false, null, undefined].includes(values)) {
-    return values
+  } else if (['', true, false, null, undefined].includes(value)) {
+    return value
   } else {
-    const number = toNumber(values)
-    if (isNaN(number)) {
-      return values
-    } else {
-      return number
-    }
+    const number = toNumber(value)
+    return isNaN(number) ? value : number
   }
 }
 
-const serialize = (values) => {
-  if (isArray(values)) {
-    return values.map(value => serialize(value))
-  } else if (isPlainObject(values)) {
-    return Object.fromEntries(Object.entries(values).map(([key, value]) => [key, serialize(value)]))
+const serialize = (value) => {
+  if (isArray(value)) {
+    return value.map(v => serialize(v))
+  } else if (isPlainObject(value)) {
+    return Object.fromEntries(Object.entries(value).map(([k, v]) => [k, serialize(v)]))
   } else {
-    if (values instanceof File) {
-      storeFile(values)
-    }
-    return values
+    return (value instanceof File) ? storeFile(value) : value
   }
 }
 
@@ -39,22 +32,23 @@ const storeFile = (file) => {
   const reader = new FileReader()
   reader.onload = function(event) {
     try {
-      localStorage.setItem(file.path, reader.result)
+      localStorage.setItem(file.name, reader.result)
     } catch (e) {
       console.log(e);
     }
   }
   reader.readAsDataURL(file)
+  return { fileName: file.name }
 }
 
-const loadFile = (filePath) => {
-  const lsString = localStorage.getItem(filePath)
+const loadFile = (fileName) => {
+  const lsString = localStorage.getItem(fileName)
   if (lsString) {
     const match = lsString.match(/^data:(?<mimeType>[a-z\/\-\+]+);\w+,(?<base64>.*?)$/)
     if (match) {
       const { mimeType, base64 } = match.groups
       const buffer = Buffer.from(base64, 'base64')
-      return new File([buffer], filePath, {type: mimeType})
+      return new File([buffer], fileName, {type: mimeType})
     }
   }
   return null
