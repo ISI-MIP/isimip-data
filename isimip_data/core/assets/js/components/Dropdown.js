b70fd41931d64026a1a80157fc06e5994cc22dfb
Jochen Klar <mail@jochenklar.de>
2025-03-24 18:14:18 +0100

    Add dropdown component and hook

---
+++ isimip_data/core/assets/js/components/Dropdown.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import { Dropdown as BootstrapDropdown } from 'bootstrap'
+
+const Dropdown = ({ dropdown, label, className, disabled, children }) => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const dropdown = new BootstrapDropdown(ref.current, {})
+    return () => dropdown.dispose()
+  }, [])
+
+  // show/hide the dropdown on when show changes
+  useEffect(() => {
+    const instance = BootstrapDropdown.getInstance(ref.current)
+    if (dropdown.display) {
+      instance.show()
+    } else {
+      instance.hide()
+    }
+  }, [dropdown.display])
+
+  // close the dropdown on a click outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdown.display && ref.current && !ref.current.contains(event.target)) {
+        dropdown.hide()
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [dropdown])
+
+  return (
+    <div ref={ref} className="dropdown dropdown-operations">
+      <button
+        type="button"
+        className={`${className} dropdown-toggle`}
+        disabled={disabled}
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+        onClick={() => dropdown.toggle()}
+      >
+        {label}
+      </button>
+      <div className="dropdown-menu">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Dropdown.propTypes = {
+  dropdown: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  children: PropTypes.node.isRequired
+}
+
+export default Dropdown
