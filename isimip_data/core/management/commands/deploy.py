4622d95d01e604a8834a2053080dd86ae87aa809
Jochen Klar <mail@jochenklar.de>
2025-04-17 13:56:16 +0200

    Add npm ci to deploy management script

--- isimip_data/core/management/commands/deploy.py
+++ isimip_data/core/management/commands/deploy.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        call_command('npm', 'ci')
         call_command('npm', 'run', 'build:prod')
         call_command('collectstatic', '--noinput', '--clear')
         call_command('clear_cache')
