a275167d6b8b3bbbc02cabf28212c1e8be6eb087
Jochen Klar <mail@jochenklar.de>
2025-06-20 11:19:36 +0200

    Add rich to dependencies and improve logging

--- config/settings/environments/local.py
+++ config/settings/environments/local.py
@@ -14,3 +14,28 @@ if DEBUG_TOOLBAR:
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
     INTERNAL_IPS = ['127.0.0.1']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'rich': {
+            'level': 'DEBUG',
+            'class': 'rich.logging.RichHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rich'],
+            'level': 'INFO'
+        },
+        'django.db.backends': {
+            'handlers': ['rich'],
+            'level': 'DEBUG',
+        },
+        'isimip_data': {
+            'handlers': ['rich'],
+            'level': 'DEBUG'
+        }
+    }
+}
