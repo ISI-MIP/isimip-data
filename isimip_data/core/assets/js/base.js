cb95c4f7603dd08710a01ee6ce83cd4aabca222b
Jochen Klar <mail@jochenklar.de>
2025-07-04 12:42:06 +0200

    Fix copy-to-clipboard

--- isimip_data/core/assets/js/base.js
+++ isimip_data/core/assets/js/base.js
@@ -1,7 +1,9 @@
-for (const element of document.getElementsByClassName('copy-to-clipboard')) {
-    element.addEventListener('click', () => {
-        const code = element.getElementsByTagName('code')[0]
-        const text = code.textContent
-        navigator.clipboard.writeText(text)
-    })
-}
+document.addEventListener('DOMContentLoaded', () => {
+    for (const element of document.getElementsByClassName('copy-to-clipboard')) {
+        element.addEventListener('click', () => {
+            const code = element.getElementsByTagName('code')[0]
+            const text = code.textContent
+            navigator.clipboard.writeText(text)
+        })
+    }
+})
