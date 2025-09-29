e5a00d60cef9d519521489500e3e14485398ea2c
Jochen Klar <mail@jochenklar.de>
2025-09-29 15:23:44 +0200

    Add DerivedInputData to home

--- isimip_data/home/views.py
+++ isimip_data/home/views.py
@@ -5,7 +5,10 @@ from isimip_data.metadata.models import Resource, Tree
 
 def home(request):
     tree = Tree.objects.using('metadata').first()
-    resources = Resource.objects.using('metadata').filter_by_tree(tree, product='DerivedOutputData')
+    resources = {
+        'derived_input_data': Resource.objects.using('metadata').filter_by_tree(tree, product='DerivedInputData'),
+        'derived_output_data': Resource.objects.using('metadata').filter_by_tree(tree, product='DerivedOutputData')
+    }
 
     return TemplateResponse(request, 'home/home.html', {
         'tree': tree,
