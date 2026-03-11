e99c7bb43077b59d2b4ac6bdcf8fe9960581d79a
Jochen Klar <mail@jochenklar.de>
2026-03-11 15:15:58 +0100

    Update requirements and refactor test setup

--- conftest.py
+++ conftest.py
@@ -1,18 +1,28 @@
-import os
+import subprocess
+from pathlib import Path
 
 import pytest
 
 from django.conf import settings
-from django.contrib.admin.utils import flatten
 from django.core.management import call_command
 
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
+    from django.db import connections
     from django.test import TestCase
-    TestCase.multi_db = True
+
+    connections['metadata'].settings_dict['NAME'] = \
+        settings.DATABASES['metadata']['TEST']['NAME']
+
     TestCase.databases = ('default', 'metadata')
 
     with django_db_blocker.unblock():
-        fixtures = flatten([os.listdir(fixture_dir) for fixture_dir in settings.FIXTURE_DIRS])
-        call_command('loaddata', *fixtures)
+        # setup the test_isimip_metadata database
+        subprocess.run(['psql', '-f', 'testing/sql/setup.sql'], check=True)
+
+        # load the fixture
+        call_command('loaddata', *[
+            str(f) for fixture_dir in settings.FIXTURE_DIRS
+            for f in Path(fixture_dir).iterdir()
+        ])
--- docs/testing.md
+++ docs/testing.md
@@ -3,22 +3,10 @@ Running tests
 
 First, remove any `CACHES` entry from `config/settings/local.py` and add `PROTOCOL_LOCATIONS = 'testing/protocol'`.
 
-Next, recreate the metadata database from the included database dump:
+Then, run the tests using `pytest`:
 
 ```bash
-createdb test_isimip_metadata
-psql test_isimip_metadata < testing/sql/test_isimip_metadata.sql
-```
-
-or shorter:
-
-```bash
-psql < testing/sql/setup.sql
-```
-
-Run the tests with the `--reuse-db` option:
-
-```bash
-pytest --reuse-db
-pytest --reuse-db --cov --cov-report html
+pytest
+pytest --reuse-db                          # keep the databases between tests
+pytest --reuse-db --cov --cov-report html  # analyse coverage and create html report
 ```
