e53d67947cfd7d3509e3f9646523a97ce0262b5b
Jochen Klar <mail@jochenklar.de>
2026-03-11 15:46:32 +0100

    Remove database setup from conftest.py again

--- conftest.py
+++ conftest.py
@@ -1,4 +1,3 @@
-import subprocess
 from pathlib import Path
 
 import pytest
@@ -18,12 +17,6 @@ def django_db_setup(django_db_setup, django_db_blocker):
     TestCase.databases = ('default', 'metadata')
 
     with django_db_blocker.unblock():
-        # setup the test_isimip_metadata database
-        sql_path = Path(__file__).resolve().parent / 'testing' / 'sql' / 'setup.sql'
-
-        subprocess.run(['psql', '-f', str(sql_path)], check=True)
-
-        # load the fixture
         call_command('loaddata', *[
             str(f) for fixture_dir in settings.FIXTURE_DIRS
             for f in Path(fixture_dir).iterdir()
